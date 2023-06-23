772813eeece8991facb391c60d05224d333fe821
Craig Rolleston <craig.rolleston@gmail.com>
2023-06-23 11:59:58 +1000

    username added to database and unique username tested

--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -1,3 +1,4 @@
+import { CreateUsernameResponse, GraphQLContext } from "../../util/types";
 
 const resolvers = {
 
@@ -6,7 +7,55 @@ const resolvers = {
     },
 
     Mutation: {
-        createUsername: () => {},
+        createUsername: async (
+                _: any, 
+                args: {username: string}, 
+                context: GraphQLContext
+            ): Promise<CreateUsernameResponse> => {
+            const { username } = args;
+            const { session, prisma } = context;
+
+            if (!session?.user) {
+                return {
+                   error: "Not authorised", 
+                };
+            }
+
+            const { id: userId } = session.user;
+
+            try {
+                // check that username is not taken
+                const existingUser = await prisma.user.findUnique({
+                   where: {
+                    username, 
+                   }  
+                })
+
+                if (existingUser) {
+                    return {
+                        error: "Username already taken. 😥 Please try another",
+                    }
+                }
+
+                // update user
+                await prisma.user.update({
+                   where: {
+                    id: userId
+                   },
+                   data: {
+                    username,
+                   },
+                });
+
+                return { success: true };
+
+            } catch (error: any) {
+                console.log("createUsername error", error);
+                return {
+                    error: error?.message,
+                };
+            }
+        },
     },
 
     // Subscription: {},
